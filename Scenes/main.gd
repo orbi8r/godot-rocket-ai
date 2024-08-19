a5043cb61b6b5c05b0e3e02bd9aa8171a5928b91
orbi8r <orbi8r+github@gmail.com>
2024-08-19 22:54:41 +0530

    added rocket forces

---
+++ Scenes/main.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@onready var generation = $CanvasLayer/Text/Generation
+@onready var coords = $CanvasLayer/Text/Coords
+@onready var velocity = $CanvasLayer/Text/Velocity
+
+@onready var rocket = $CanvasLayer/Rocket
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.text = "Velocity : " + str(rocket.linear_velocity)
+	coords.text = "Coordinate : " + str(rocket.global_position)
+	
+	generation.text = str(rocket.thrust_dir) + str(rocket.is_trusting)
--- Scenes/rocket.gd
+++ Scenes/rocket.gd
@@ -1,5 +1,11 @@
 extends RigidBody3D
 
+@onready var thrust_point = $ThrustPoint
+@onready var thruster = $ThrustPoint/Thruster
+
+var is_trusting = false
+var thrust_dir = Vector3(0,15,0)
+var thrust_xz_dir = Vector3(1,0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +14,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	if Input.is_action_pressed("clockwise"):
+		thrust_point.rotate_y(deg_to_rad(-10))
+		thrust_dir = thrust_dir.rotated(Vector3(0,1,0), deg_to_rad(-10))
+		thrust_xz_dir = thrust_xz_dir.rotated(Vector3(0,1,0), deg_to_rad(-10))
+	elif Input.is_action_pressed("anticlockwise"):
+		thrust_point.rotate_y(deg_to_rad(10))
+		thrust_dir = thrust_dir.rotated(Vector3(0,1,0), deg_to_rad(10))
+		thrust_xz_dir = thrust_xz_dir.rotated(Vector3(0,1,0), deg_to_rad(10))
+		
+		
+	if Input.is_action_pressed("exterior") and thruster.rotation_degrees.x < 60:
+		thruster.rotate_x(deg_to_rad(3))
+		thrust_dir = thrust_dir.rotated(thrust_xz_dir, deg_to_rad(3))
+	elif Input.is_action_pressed("interior") and thruster.rotation_degrees.x > 0:
+		thruster.rotate_x(deg_to_rad(-3))
+		thrust_dir = thrust_dir.rotated(thrust_xz_dir, deg_to_rad(-3))
+		
+	if Input.is_action_just_pressed("thrust"):
+		is_trusting = true
+	elif Input.is_action_just_released("thrust"):
+		is_trusting = false
+	
+	if is_trusting == true:
+		apply_force(thrust_dir, Vector3(0,-5,0))
+	
