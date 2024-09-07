810eb4a193e649212e3e2b3e39b6dd946e792fc4
orbi8r <orbi8r+github@gmail.com>
2024-09-07 16:41:29 +0530

    changes to thrust system

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,15 +3,21 @@ extends Node3D
 @onready var generation = $CanvasLayer/Text/Generation
 @onready var coords = $CanvasLayer/Text/Coords
 @onready var velocity = $CanvasLayer/Text/Velocity
+@onready var rotationn = $CanvasLayer/Text/Rotation
 
 @onready var rocket = $CanvasLayer/Rocket
 
 @export var rocketheight = 300
+var gen = 0
 
 func reset():
 	rocket.global_position = Vector3(randi_range(-250,250), rocketheight, randi_range(-250,250))
 	rocket.linear_velocity = Vector3.ZERO
+	rocket.angular_velocity = Vector3.ZERO
 	rocket.rotation = Vector3.ZERO
+	gen+=1
+	generation.text = "Gen : " + str(gen)
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,6 +26,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity.text = "Velocity : " + str(rocket.linear_velocity)
-	coords.text = "Coordinate : " + str(rocket.global_position)
+	velocity.text = "Velocity : " + str(round(rocket.linear_velocity))
+	coords.text = "Coordinate : " + str(round(rocket.global_position))
+	rotationn.text = "Rotation : " + str(round(rocket.rotation_degrees))
+
+	
+	if rocket.global_position.y < -20:
+		reset()
 
--- Scenes/rocket.gd
+++ Scenes/rocket.gd
@@ -6,12 +6,8 @@ extends RigidBody3D
 @onready var ai_controller_3d = $"../AIController3D"
 
 
-@export var thrustforce = 15
-
 
 var is_trusting = false
-var thrust_dir = Vector3(0,thrustforce,0)
-var thrust_xz_dir = Vector3(1,0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,12 +20,9 @@ func _process(_delta):
 	# AI inputs
 	
 	thrust_point.rotate_y(deg_to_rad(10 * ai_controller_3d.thrustortho))
-	thrust_dir = thrust_dir.rotated(Vector3(0,1,0), deg_to_rad(10 * ai_controller_3d.thrustortho))
-	thrust_xz_dir = thrust_xz_dir.rotated(Vector3(0,1,0), deg_to_rad(10 * ai_controller_3d.thrustortho))
 	
 	if thruster.rotation_degrees.x <= 60 and thruster.rotation_degrees.x >= 0:
 		thruster.rotate_x(deg_to_rad(3 * ai_controller_3d.thrustrotation))
-		thrust_dir = thrust_dir.rotated(thrust_xz_dir, deg_to_rad(3 * ai_controller_3d.thrustrotation))
 	elif thruster.rotation_degrees.x > 60:
 		thruster.rotation_degrees.x = 59
 	elif thruster.rotation_degrees.x < 0:
@@ -39,20 +32,14 @@ func _process(_delta):
 	
 	if Input.is_action_pressed("clockwise"):
 		thrust_point.rotate_y(deg_to_rad(-10))
-		thrust_dir = thrust_dir.rotated(Vector3(0,1,0), deg_to_rad(-10))
-		thrust_xz_dir = thrust_xz_dir.rotated(Vector3(0,1,0), deg_to_rad(-10))
 	elif Input.is_action_pressed("anticlockwise"):
 		thrust_point.rotate_y(deg_to_rad(10))
-		thrust_dir = thrust_dir.rotated(Vector3(0,1,0), deg_to_rad(10))
-		thrust_xz_dir = thrust_xz_dir.rotated(Vector3(0,1,0), deg_to_rad(10))
 		
 		
 	if Input.is_action_pressed("exterior") and thruster.rotation_degrees.x < 60:
 		thruster.rotate_x(deg_to_rad(3))
-		thrust_dir = thrust_dir.rotated(thrust_xz_dir, deg_to_rad(3))
 	elif Input.is_action_pressed("interior") and thruster.rotation_degrees.x > 0:
 		thruster.rotate_x(deg_to_rad(-3))
-		thrust_dir = thrust_dir.rotated(thrust_xz_dir, deg_to_rad(-3))
 		
 	# Thrust firing for Human and AI controls	
 	
@@ -64,5 +51,6 @@ func _process(_delta):
 		fire.visible = false
 	
 	if is_trusting == true:
-		apply_force(thrust_dir, Vector3(0,-5,0))
+		apply_force(30*(thruster.global_position - fire.global_position), fire.global_position - global_position)
+		
 	
